af42c22babe1b7ee7a319925a21b774d825d2581
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-05-12 16:32:21 +0200

    Add Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+export function Header() {
+  return (
+    <div className={styles.box}>
+      <h1 className={styles.text}>Portfolio</h1>
+    </div>
+  );
+}
