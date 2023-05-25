c802b85fce1b44d614a0a6b022211ce1c41c4624
Adriana <97173869+a-zajonc@users.noreply.github.com>
2023-05-25 13:04:12 +0200

    Update About Section

--- src/components/About/Technologies/IconsRender/iconData.tsx
+++ src/components/About/Technologies/IconsRender/iconData.tsx
@@ -563,7 +563,7 @@ export const Icons = [
         viewBox="0 0 512 512"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path fill-rule="evenodd" d="M256,48,496,464H16Z" />
+        <path fillRule="evenodd" d="M256,48,496,464H16Z" />
       </svg>
     ),
     darkIcon: (
@@ -574,7 +574,7 @@ export const Icons = [
         viewBox="0 0 512 512"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path fill-rule="evenodd" d="M256,48,496,464H16Z" />
+        <path fillRule="evenodd" d="M256,48,496,464H16Z" />
       </svg>
     ),
   },
